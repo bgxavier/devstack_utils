1bfaed7fb747a8f13e956251e05c9a1d91ce9a4d
Bruno Xavier <bgxavier@gmail.com>
2015-06-02 22:26:06 -0300

    setup devstack.sh

---
+++ setup_devstack.sh
@@ -0,0 +1,12 @@
+BRANCH=stable/kilo
+
+useradd -m stack
+echo "stack	ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "Defaults:stack !requiretty" >> /etc/sudoers
+DEBIAN_FRONTEND=noninteractive sudo apt-get -qqy update || sudo yum update -qy
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy git || sudo yum install -qy git
+cd /home/stack
+sudo -u stack git clone https://git.openstack.org/openstack-dev/devstack
+cd devstack
+git checkout $BRANCH
+cp /vagrant/local.conf .
