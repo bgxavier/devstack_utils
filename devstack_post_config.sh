38dda0d04bdf8e4ee40e8cda8c257f0824e16d12
Bruno Xavier <bgxavier@gmail.com>
2015-07-27 22:36:07 -0300

    post config

--- devstack_post_config.sh
+++ devstack_post_config.sh
@@ -1,6 +1,4 @@
-#/bin/bash
-
-source /opt/devstack/openrc admin admin
+. /opt/devstack/openrc admin admin
 
 nova quota-class-update --instances -1 --cores -1 --ram -1 --fixed-ips -1 --floating-ips -1 default
 nova flavor-create --ephemeral 0  m1.micro 6 512 4 1
@@ -8,3 +6,6 @@ nova-manage service disable --host=controller --service=nova-compute
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
 
+glance image-create --name “osv-tomcat” --is-public True --disk-format qcow2 --container-format bare --file /opt/images/osv-tomcat-resized.qcow2
+
+glance image-create --name “osv-tomcat” --is-public True --disk-format qcow2 --container-format bare --file /opt/images/ubuntu-tomcat.qcow2
