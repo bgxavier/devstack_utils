162a92d7cb374d2d579d25410406bfa044506f74
Bruno Xavier <bgxavier@gmail.com>
2015-07-30 00:34:59 -0300

    collect_osprofiler

---
+++ coleta_osprofiler.sh
@@ -0,0 +1,47 @@
+virt_type=$1
+num_instances=$2
+
+KVM_IMAGE="32a54fbf-8a22-489c-8eb5-bf51f85112fa"
+DOCKER_IMAGE="00f9dd6d-3d96-4fc2-85c8-02caf0d553da"
+OSV_IMAGE="4eec9e83-15b9-4a3d-8641-c79544d2f2e5"
+
+case $1 in
+
+kvm)
+        IMAGE=$KVM_IMAGE
+;;
+
+docker)
+
+        IMAGE=$DOCKER_IMAGE
+;;
+
+osv)
+        IMAGE=$OSV_IMAGE
+
+;;
+
+*)
+  echo "kvm, osv or docker"
+  ;;
+
+esac
+
+echo "Changing to admin tenant.."
+
+source /opt/devstack/openrc admin admin
+
+echo "Collecting data to $1_osprofiler.csv"
+
+for i in { 1..'$num_instances' }; do nova --profile SECRET_KEY boot --image $IMAGE --flavor 6 test | egrep -o "html.*" | osprofiler trace show --html `awk '{print $2}'` | grep -o '"started": 0, "finished": [0-9]*, "name": "total"' | egrep -o '"finished": [0-9]*' | egrep -o [0-9]* | tee -a $1_osprofiler.csv; done
+
+echo "Done."
+
+echo "Removing instances..."
+nova list --all | awk '$2 && $2 != "ID" {print $2}' | xargs -n1 nova delete
+
+echo "Going back to demo tenant."
+
+source /opt/devstack/openrc demo demo
+
+echo "Done."
