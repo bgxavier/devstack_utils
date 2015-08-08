a721f88123d993e039ce77105c496ae2efbbc54a
Bruno Xavier <bgxavier@gmail.com>
2015-08-08 01:24:29 +0000

    vai utils

--- scripts/coleta_osprofiler_total.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-coleta_osprofiler.sh kvm 5; coleta_osprofiler.sh kvm 10; coleta_osprofiler.sh kvm 20; coleta_osprofiler.sh kvm 30;coleta_osprofiler.sh osv 5;coleta_osprofiler.sh osv 10;coleta_osprofiler.sh osv 20; coleta_osprofiler.sh osv 30
