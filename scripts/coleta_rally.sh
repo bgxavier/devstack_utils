d43d88633a914bdef18595ec5705eba4f54ffec8
Bruno Xavier <bgxavier@gmail.com>
2015-08-08 01:45:12 +0000

    vai utils

--- scripts/coleta_rally.sh
+++ scripts/coleta_rally.sh
@@ -1,27 +1,34 @@
 virt_type=$1
 num_instances=$2
 
-KVM_BOOT="/opt/openstack_benchmarks/kvm_boot.yaml"
-OSV_BOOT="/opt/openstack_benchmarks/osv_boot.yaml"
-DOCKER_BOOT="/opt/openstack_benchmarks/docker_boot.yaml"
+KVM_IMAGE="cirros-0.3.2-x86_64-uec"
+OSV_IMAGE="osv-tomcat"
+DOCKER_IMAGE="tomcat"
+
+FLAVOR="m1.tiny"
+
 OUTPUT_TEMP='osprofiler.txt'
 OUTPUT_TIMES='ostimes.csv'
+BOOT_JSON_FILE='boot.json'
+
 RALLY="/usr/local/bin/rally"
 
 case $1 in
 
 kvm)
         BOOT_YAML=$KVM_BOOT
+        IMAGE=$KVM_IMAGE
 ;;
 
 docker)
 
         BOOT_YAML=$DOCKER_BOOT
+        IMAGE=$DOCKER_IMAGE
 ;;
 
 osv)
         BOOT_YAML=$OSV_BOOT
-
+        IMAGE=$OSV_IMAGE
 ;;
 
 *)
@@ -29,6 +36,8 @@ osv)
   ;;
 
 esac
+
+BOOT_JSON='{"NovaServers.boot_server":[{"args":{"flavor":{"name":"'$FLAVOR'"},"image":{"name":"'$IMAGE'"}},"runner":{"type":"constant","times":'$num_instances',"concurrency":'$num_instances'},"context":{"users":{"tenants":1,"users_per_tenant":1}}}]}'
 
 source /opt/devstack/openrc admin admin
 
@@ -41,10 +50,10 @@ docker rm $(docker ps -a -q) > /dev/null 2>&1
 docker rmi $(docker images -q) > /dev/null 2>&1 
 
 echo "Running rally and collecting data to ./$OUTPUT_TEMP"
-#> $OUTPUT_TEMP
-#> $OUTPUT_TIMES
 
-$RALLY task start $BOOT_YAML | grep osprofiler | awk '{print $5}' > $OUTPUT_TEMP
+echo $BOOT_JSON > $BOOT_JSON_FILE
+
+$RALLY task start ./$BOOT_JSON_FILE | grep osprofiler | awk '{print $5}' > $OUTPUT_TEMP
 echo "total_time;spawn_time;image_time;domain_time" > $OUTPUT_TIMES
 
 for i in `cat $OUTPUT_TEMP`; do
