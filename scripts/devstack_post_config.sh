599cafcc1c68696c4300361ad55b7576fb703856
Bruno Xavier <bgxavier@gmail.com>
2016-01-11 15:56:48 -0200

    .

--- scripts/devstack_post_config.sh
+++ scripts/devstack_post_config.sh
@@ -7,10 +7,10 @@ nova-manage service disable --host=controller --service=nova-compute
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
 
-glance image-create --name “osv-tomcat” --is-public True --disk-format qcow2 --container-format bare --file /opt/images/osv-tomcat-resized.qcow2
+#glance image-create --name “osv-tomcat” --is-public True --disk-format qcow2 --container-format bare --file /opt/images/osv-tomcat-resized.qcow2
 
-glance image-create --name “ubuntu-tomcat” --is-public True --disk-format qcow2 --container-format bare --file /opt/images/ubuntu-tomcat.qcow2
+#glance image-create --name “ubuntu-tomcat” --is-public True --disk-format qcow2 --container-format bare --file /opt/images/ubuntu-tomcat.qcow2
 
-glance image-create --name tomcat --is-public true --container-format docker --disk-format raw --file /opt/images/tomcat.tar
+#glance image-create --name tomcat --is-public true --container-format docker --disk-format raw --file /opt/images/tomcat.tar
 
 sudo cp /opt/devstack_utils/controller/etc/neutron/api-paste.ini /etc/neutron/
