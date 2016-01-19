fec75a98e69d7b0ec07ac7e9353634d5fab12a87
Bruno Xavier <bgxavier@gmail.com>
2016-01-19 15:58:21 -0200

    vai

--- scripts/setup_nova_docker.sh
+++ scripts/setup_nova_docker.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y python-pip python-dev
 
 rm -rf /opt/stack/nova-docker
 sudo mkdir -p /opt/stack
-sudo git clone https://git.openstack.org/stackforge/nova-docker /opt/stack/nova-docker
+sudo git clone https://github.com/openstack/nova-docker.git /opt/stack/nova-docker
 cd /opt/stack/nova-docker
 # Check out a different version if not using master, i.e:
 sudo git checkout stable/kilo && sudo git pull --ff-only origin stable/kilo
