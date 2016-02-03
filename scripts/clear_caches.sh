aa3c1e61d32e74f2484c912d4928ec928bd13cfb
Bruno Xavier <bgxavier@gmail.com>
2016-02-03 21:38:19 -0200

    a

--- scripts/clear_caches.sh
+++ scripts/clear_caches.sh
@@ -1,3 +1,4 @@
-sudo rm -rfv /opt/stack/data/nova/instances/_base/*
+#sudo rm -rfv /opt/stack/data/nova/instances/_base/*
+find /opt/stack/data/nova/instances/_base/* -maxdepth 0 -not -name cloudius -exec rm -rf {} \;
 sudo rm -rfv /opt/stack/data/unikernel/*
 #sudo rm -rfv /opt/stack/data/glance/images/*
