33049d751be9408c4d6dbe64c0df53d0cd3f4e6a
Bruno Xavier <bgxavier@gmail.com>
2016-02-03 22:57:27 -0200

    .

--- scripts/devstack_post_config.sh
+++ scripts/devstack_post_config.sh
@@ -2,7 +2,7 @@
 
 nova quota-class-update --instances -1 --cores -1 --ram -1 --fixed-ips -1 --floating-ips -1 default
 nova flavor-create --ephemeral 0  m1.micro 6 512 4 1
-nova flavor-create --ephemeral 0  m2.small 7 512 20 1
+nova flavor-create --ephemeral 0  m2.small 7 512 12 1
 nova-manage service disable --host=controller --service=nova-compute 
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
